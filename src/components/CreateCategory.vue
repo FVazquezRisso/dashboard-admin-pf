73a3c87906eaa469f7f26580f28e0f64d75e9775
FVazquezRisso <facundovazquez321@gmail.com>
2023-08-18 10:07:50 -0300

    Se separó nuevamente en objeto de rutas

---
+++ src/components/CreateCategory.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue';
+
+const isCreating = ref(false)
+</script>
+
+<template>
+  <div class="create-button-container">
+    <button class="btn">Create Category</button>
+  </div>
+</template>
+
+<style scoped>
+.create-button-container {
+  display: flex;
+  width: 550px;
+  margin: 1rem;
+  padding: .25rem;
+  border: 2px solid white;
+  border-radius: 1rem;
+}
+
+.btn {
+  width: 100%;
+  margin: 0 auto;
+  background-color: transparent;
+  border-color: transparent;
+}
+
+.btn:hover {
+  color: rgb(81, 190, 81);
+}
+
+.btn:focus {
+  outline: none;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,31 +1,8 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import { routes } from "./Routes";
 import { createRouter, createWebHashHistory } from "vue-router";
-
-import LoginPage from "./pages/LoginPage.vue";
-import UsersPage from "./pages/UsersPage.vue";
-import SongsPage from "./pages/SongsPage.vue";
-import CategoriesPage from "./Pages/CategoriesPage.vue";
-
-const routes = [
-  {
-    path: "/",
-    component: LoginPage,
-  },
-  {
-    path: "/users",
-    component: UsersPage,
-  },
-  {
-    path: "/songs",
-    component: SongsPage,
-  },
-  {
-    path: "/categories",
-    component: CategoriesPage,
-  },
-];
 
 const router = createRouter({
   history: createWebHashHistory(),
