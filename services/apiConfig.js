567c9a830d20404a9306ab17e046d54d5b1ce4a1
FVazquezRisso <facundovazquez321@gmail.com>
2023-08-23 09:25:09 -0300

    Deploy

--- services/apiConfig.js
+++ services/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const spotyFansApi = axios.create({
-  // baseURL: "https://spootyfans.onrender.com",
-  baseURL: "http://localhost:3001",
+  baseURL: "https://spootyfans.onrender.com",
+  // baseURL: "http://localhost:3001",
 });
